fc2618624af66dce60addd1f898df21e93f7e2b9
Asche Jurotich <asche.jurotich@gmail.com>
2024-08-12 03:06:39 -0500

    Implemented Entity Framework

---
+++ StoreFront.Data.EF/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.Data.EF.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Block> Blocks { get; set; } = new List<Block>();
+}
---
+++ StoreFront.Data.EF/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.Data.EF.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int BuyerId { get; set; }
+
+    public int SupplierId { get; set; }
+
+    public DateTime? DateOrdered { get; set; }
+
+    public bool DeliverToBuyerLocation { get; set; }
+
+    public string? DeliveryCoords { get; set; }
+
+    public virtual User Buyer { get; set; } = null!;
+
+    public virtual ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
+
+    public virtual User Supplier { get; set; } = null!;
+}
